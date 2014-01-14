fbbd0fe3f6590d235354ce92e763a10c5469858f
唐老鸭光光 <gytang1986@gmail.com>
2014-01-14 22:29:38 +0800

    bigpipe

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+
+/*
+ * GET home page.
+ */
+module.exports = function(app){
+	app.get('/',function(req,res){
+    // res.render('index', { 
+    //   title:"测试"
+    // });
+
+    res.render('index', {
+        title:"测试"
+      },function (err, str) {
+      //res.setHeader('content-type', 'text/html; charset=utf-8')
+      res.write(str)
+      // res.write('<section id="s1"></section><section id="s2"></section>')
+    })
+    var data={is:"true"};
+    setTimeout(function(){
+      res.write('<script>bigpipe.set("test",'+JSON.stringify(data)+');</script>');
+      res.end();
+    }, 5000);
+  });
+}
